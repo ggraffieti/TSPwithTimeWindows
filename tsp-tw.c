7891600a6539d124f446b44527be2e90c767e0c8
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2018-05-30 21:02:15 +0200

    fix bug in depoit return loop

--- tsp-tw.c
+++ tsp-tw.c
@@ -38,8 +38,8 @@ int gGamma[N+1][TIMES];
 int fCurrentCustomer[N+1][TIMES];
 
 int getHead(int k, int i) {
-  if (k == N+1 && i == 0) {
-    return head[N*N];
+  if (k == N+1) {
+    return head[(N*N) + i];
   } else if (k < N+1 && i == N+1) {
     int index = ((k-1)*N) + (i-2);
     while (head[index] != 0) {
@@ -52,8 +52,8 @@ int getHead(int k, int i) {
 }
 
 void setHead(int k, int i, int pointer) {
-  if (k == N+1 && i == 0) {
-    head[N*N] = pointer;
+  if (k == N+1) {
+    head[(N*N) + i] = pointer;
   } else {
     head[((k-1)*N) + (i-1)] = pointer;
   }
@@ -124,12 +124,22 @@ void printPath(int state, int g) {
 }
 
 void printStates(int k) {
-  for (int i = getHead(k, 1); i < getHead(k+1,1); i++) {
-    printf("%d\n", i);
-    printf("F = %lf, T = %d, Pi = %d, G = %lf, Gamma = %d\n", F[i], stateTime[i], Pi[i], G[i], Gamma[i]);
-    printPath(i, 0);
-    printf("\n");
+  if (k < N + 1) {
+    for (int i = getHead(k, 1); i < getHead(k+1, k == N ? 0 : 1); i++) {
+      printf("%d\n", i);
+      printf("F = %lf, T = %d, Pi = %d, G = %lf, Gamma = %d\n", F[i], stateTime[i], Pi[i], G[i], Gamma[i]);
+      printPath(i, 0);
+      printf("\n");
+    }
+  } else {
+    for (int i = getHead(k, 0); i < getHead(k, 1); i++) {
+      printf("%d\n", i);
+      printf("F = %lf, T = %d, Pi = %d, G = %lf, Gamma = %d\n", F[i], stateTime[i], Pi[i], G[i], Gamma[i]);
+      printPath(i, 0);
+      printf("\n");
+    }
   }
+
 }
 
 int main() {
@@ -173,9 +183,9 @@ int main() {
     }
     checkDominance();
     /* deploy states */
-    for (int p = 1; p <= N; p++) {
+    for (int p = 1; p <= N; p++) { // p = last customer.
       setHead(k, p, z);
-      for (int l = 0; l < TIMES; l++) {
+      for (int l = 0; l < TIMES; l++) { // l = time
         if (f[p][l] != INFINITY) {
           stateTime[z] = l;
           F[z] = f[p][l];
@@ -188,7 +198,53 @@ int main() {
       }
     }
   }
-  setHead(4, 1, z);
-  printStates(3);
+
+  int k = N + 1;
+  int i = 0;
+  resetFG();
+  for (int j = 1; j <= N; j++) { /* for every final destination in previous states */
+    for (int h = getHead(k-1, j); h < getHead(k-1, j+1); h++) { /* h is the previous state considered now */
+      int time = intMax(earlyTime[i], stateTime[h] + t[j][i]);
+      double cost;
+      if (i != currentCustomer[Pi[h]]) {
+        cost = F[h] + c[j][i] + delayCost(i, time);
+      } else {
+        cost = G[h] + c[j][i] + delayCost(i, time);
+      }
+
+      if (cost <= f[i][time] && cost != INFINITY) {
+        g[i][time] = f[i][time];
+        gGamma[i][time] = fPi[i][time];
+        f[i][time] = cost;
+        fPi[i][time] = h;
+        fCurrentCustomer[i][time] = i;
+      }
+    }
+  }
+  checkDominance();
+  /* deploy states */
+  setHead(k, i, z);
+  for (int l = 0; l < TIMES; l++) { // l = time
+    if (f[i][l] != INFINITY) {
+      stateTime[z] = l;
+      F[z] = f[i][l];
+      G[z] = g[i][l];
+      Pi[z] = fPi[i][l];
+      currentCustomer[z] = fCurrentCustomer[i][l];
+      Gamma[z] = gGamma[i][l];
+      z++;
+    }
+  }
+  setHead(N+1, 1, z);
+  printStates(4);
+  // for (int k = 1; k <= N+1; k++) {
+  //   if (k == N+1) {
+  //     printf("K = %d, i = %d, head = %d\n", k, 0, getHead(k, 0));
+  //   } else {
+  //     for (int i = 1; i <= N; i++) {
+  //       printf("K = %d, i = %d, head = %d\n", k, i, getHead(k, i));
+  //     }
+  //   }
+  // } 
 
 }
